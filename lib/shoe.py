1cccf370dcec35ef284dc2db6d45ef5f4a2db589
Haron M Maina <haronmmaina@Harons-MacBook-Air.local>
2025-05-13 11:50:29 +0300

    Makes changes

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand=brand
+        self.size=size
+        self.condition= None
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self,size):
+        if not isinstance(size,int):
+            print("size must be an integer")
+            self._size="Invalid size"
+        else:
+            self._size=size
+
+        
+
+
